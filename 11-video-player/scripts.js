b3c7f6848bafc632bd33b08985605eafaafb281f
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2023-01-26 10:21:00 -0600

    Added togglePlay function to toggle video pause/play when clicked

--- 11-video-player/scripts.js
+++ 11-video-player/scripts.js
@@ -5,3 +5,18 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelector('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+    //Version one
+    // if (video.paused) {
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+    //Version two
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+    // Version three
+    // video[video.paused ? 'play' : 'pause']();
+};
+video.addEventListener('click', togglePlay);
