048562081ba29e39abcbe0077d403dadfd03ab88
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2023-01-26 11:04:44 -0600

    Added scrub function to allow drag and drop of progress bar.

--- 11-video-player/scripts.js
+++ 11-video-player/scripts.js
@@ -40,6 +40,10 @@ function handleProgress() {
     progressBar.style.flexBasis = `${percent}%`
 };
 
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -48,3 +52,10 @@ video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+progress.addEventListener('mouseout', () => mousedown = false);
+
