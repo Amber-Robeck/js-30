fb834d7f6327695d23d96ff641503944cbec76b5
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2023-01-28 19:36:35 -0600

    Added new solution to camera not working, readme.md with link to stack

---
+++ 19-webcam-fun/readme.md
@@ -0,0 +1,3 @@
+# NOTES
+
+[Error on stack](https://stackoverflow.com/questions/27120757/failed-to-execute-createobjecturl-on-url) Solution wasn't pulling up video I researched error message `caught (in promise) TypeError: Failed to execute 'createObjectURL' on 'URL': Overload resolution failed.` and found this article
--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -3,3 +3,16 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            // console.log(localMediaStream);
+            // localMediaStream = new MediaStream();
+            // video.src = localMediaStream;
+            // window.URL.createObjectURL(localMediaStream);
+            video.srcObject = localMediaStream;
+            video.play();
+        });
+};
+getVideo();
