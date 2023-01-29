a14dfaa7233c1b772a8a0f71756fd303535ef41c
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2023-01-28 20:02:25 -0600

    Added functionality for take picture and download image

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -13,6 +13,38 @@ function getVideo() {
             // window.URL.createObjectURL(localMediaStream);
             video.srcObject = localMediaStream;
             video.play();
+        })
+        .catch(err => {
+            console.error("didn't agree", err);
         });
 };
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    // console.dir(video)
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height)
+    }, 16);
+};
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    // console.log(data);
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'newPicture');
+    link.textContent = 'Download Image';
+    strip.insertBefore(link, strip.firstChild);
+
+};
+
 getVideo();
+video.addEventListener('canplay', paintToCanvas);
+// paintToCanvas();  why doesn't this call it but in browser window it works????
